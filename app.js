c07843c0e49ba91e7d70b74362240f35636b908d
urvashi02sinha <65072659+urvashi02sinha@users.noreply.github.com>
2020-08-03 20:16:13 +0530

    changes on food.ejs
    styling food.ejs using bootstrap and adding some more images into app.js.

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.get("/", function(req,res){
 });
 app.get("/food", function(req,res){
   var food = [
-      {name: "Pizza" ,image:"https://www.tasteofhome.com/wp-content/uploads/2017/10/Chicken-Pizza_exps30800_FM143298B03_11_8bC_RMS-2.jpg"},
+      {name: "Pizza" ,image:"http://4.bp.blogspot.com/-n-jZjyEzncE/Uq8IxN6-giI/AAAAAAAADWk/OL-YhSPEG_4/s1600/Pizza+Food+Hd+Wallpaper.jpg"},
+      {name: "Italian Pasta" ,image:"https://1.bp.blogspot.com/-0IwBC-Wltc8/W8_d-FuHaXI/AAAAAAAAC0o/wm0Hs1FmHjklNnUlXoCCmtbqnpqlwv1fQCLcBGAs/s1600/World%2BPasta%2BDay%2B2018.jpg"},
+      {name: "Noodles" ,image:"http://new.nankaseimen.com/wp-content/uploads/2016/10/Nanka-0059.jpg"},
+      {name: "Pizza" ,image:"http://4.bp.blogspot.com/-n-jZjyEzncE/Uq8IxN6-giI/AAAAAAAADWk/OL-YhSPEG_4/s1600/Pizza+Food+Hd+Wallpaper.jpg"},
+      {name: "Italian Pasta" ,image:"https://1.bp.blogspot.com/-0IwBC-Wltc8/W8_d-FuHaXI/AAAAAAAAC0o/wm0Hs1FmHjklNnUlXoCCmtbqnpqlwv1fQCLcBGAs/s1600/World%2BPasta%2BDay%2B2018.jpg"},
+      {name: "Noodles" ,image:"http://new.nankaseimen.com/wp-content/uploads/2016/10/Nanka-0059.jpg"},
+      {name: "Pizza" ,image:"http://4.bp.blogspot.com/-n-jZjyEzncE/Uq8IxN6-giI/AAAAAAAADWk/OL-YhSPEG_4/s1600/Pizza+Food+Hd+Wallpaper.jpg"},
       {name: "Italian Pasta" ,image:"https://1.bp.blogspot.com/-0IwBC-Wltc8/W8_d-FuHaXI/AAAAAAAAC0o/wm0Hs1FmHjklNnUlXoCCmtbqnpqlwv1fQCLcBGAs/s1600/World%2BPasta%2BDay%2B2018.jpg"},
       {name: "Noodles" ,image:"http://new.nankaseimen.com/wp-content/uploads/2016/10/Nanka-0059.jpg"},
       {name: "Aalu paratha" ,image:"https://i.ytimg.com/vi/ohrbhGYQ-1o/maxresdefault.jpg"}
