fbd5772789b2c420164129c2895f4f15634b659a
namank63 <namank63@gmail.com>
2020-08-08 02:37:17 +0530

    Show Route Setup
    Refactor name from recipes.ejs to index.ejs
    Added description to recipe model
    Added a show route/templet

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ SCHEMA SETUP
 **********************************************************/
 var recipeSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 //compiling schema into model
@@ -29,7 +30,8 @@ var Recipe = mongoose.model("Recipe", recipeSchema);
 // Recipe.create(
 //     { 
 //         name: "Italian Pasta", 
-//         image: "https://1.bp.blogspot.com/-0IwBC-Wltc8/W8_d-FuHaXI/AAAAAAAAC0o/wm0Hs1FmHjklNnUlXoCCmtbqnpqlwv1fQCLcBGAs/s1600/World%2BPasta%2BDay%2B2018.jpg"
+//         image: "https://1.bp.blogspot.com/-0IwBC-Wltc8/W8_d-FuHaXI/AAAAAAAAC0o/wm0Hs1FmHjklNnUlXoCCmtbqnpqlwv1fQCLcBGAs/s1600/World%2BPasta%2BDay%2B2018.jpg",
+//         description: "It is a awesome italian Pasta"
 //     },  function(err, recipe){
 //         if(err) {
 //             console.log(err);
@@ -47,22 +49,25 @@ app.get("/", function(req,res){
     res.render("landing");
 });
 
+//INDEX ROUTE: shows all recipes
 app.get("/recipes", function(req,res){
     //Get all recipes from DB
     Recipe.find({}, function(err, allRecipes){
         if(err) {
             console.log(err);
         } else {
-            res.render("recipes", { recipes: allRecipes});
+            res.render("index", { recipes: allRecipes});
         }
     });
 });
 
+//CREATE ROUTE: add new recipe to DB
 app.post("/recipes", function(req, res){
     //get data from form and add to recpies array
     var name = req.body.name;
     var image = req.body.image;
-    var newRecpie = {name: name, image: image}
+    var description = req.body.description;
+    var newRecpie = {name: name, image: image, description: description}
     //create a new recipe and save to DB
     Recipe.create(newRecpie, function(err, newlyCreated){
         if(err) {
@@ -74,8 +79,22 @@ app.post("/recipes", function(req, res){
     });
 });
 
+//NEW ROUTE: show form to create new recipe
 app.get("/recipes/new",function(req,res){
     res.render("new.ejs");
+});
+
+//SHOW ROUTE: shows more info about one recipe
+app.get("/recipes/:id", function(req, res) {
+    //find the recipe with provided ID
+    Recipe.findById(req.params.id, function(err, foundRecipe){
+        if(err) {
+            console.log(err);
+        } else {
+            //render show template with that recipe
+            res.render("show", {recipe: foundRecipe});
+        }
+    });
 });
  
 app.listen(3000,function(){
