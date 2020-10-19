012d28c77e9e26beb8ae84e2c84900e3405e7aef
namank63 <48546593+namank63@users.noreply.github.com>
2020-10-19 09:49:02 +0530

    Code refactor and cleanup
    -Moved all routes to seperate file and clean app.js by just requiring the routes

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ var express         = require("express"),
     seedDB          = require("./seeds");
     
     
-
+    
+/*********************************************************
+REQUIRING ROUTES
+**********************************************************/
+var commentRoutes = require("./routes/comments"),
+    recipeRoutes  = require("./routes/recipes"),
+    indexRoutes   = require("./routes/index");
+    
+    
+    
 /*********************************************************
 CONFIGURATIONS
 **********************************************************/
@@ -50,154 +59,10 @@ app.use(function(req, res, next){
     next();
 });
 
+app.use(indexRoutes);
+app.use("/recipes", recipeRoutes);
+app.use("/recipes/:id/comments", commentRoutes);
 
-
-/*********************************************************
-ROUTES
-**********************************************************/
-//ROOT ROUTE: landing page
-app.get("/", function(req,res){
-    res.render("landing");
-});
-
-//INDEX ROUTE: shows all recipes
-app.get("/recipes", function(req,res){
-    //Get all recipes from DB
-    Recipe.find({}, function(err, allRecipes){
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("recipes/index", { recipes: allRecipes});
-        }
-    });
-});
-
-//CREATE ROUTE: add new recipe to DB
-app.post("/recipes", function(req, res){
-    //get data from form and add to recpies array
-    var name = req.body.name;
-    var image = req.body.image;
-    var description = req.body.description;
-    var newRecpie = {name: name, image: image, description: description}
-    //create a new recipe and save to DB
-    Recipe.create(newRecpie, function(err, newlyCreated){
-        if(err) {
-            console.log(err);
-        } else {
-            //redirect back to recpies page
-            res.redirect("/recipes");
-        }
-    });
-});
-
-//NEW ROUTE: show form to create new recipe
-app.get("/recipes/new",function(req,res){
-    res.render("recipes/new");
-});
-
-//SHOW ROUTE: shows more info about one recipe
-app.get("/recipes/:id", function(req, res) {
-    //find the recipe with provided ID
-    Recipe.findById(req.params.id).populate("comments").exec( function(err, foundRecipe){
-        if(err) {
-            console.log(err);
-        } else {
-             console.log(foundRecipe);
-            //render show template with that recipe
-            res.render("recipes/show", {recipe: foundRecipe});
-        }
-    });
-});
-
-
-
-/*********************************************************
-COMMENT ROUTES
-**********************************************************/
-app.get("/recipes/:id/comments/new", isLoggedIn,function(req, res){
-    //find recipe by id
-    Recipe.findById(req.params.id, function(err, recipe){
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", {recipe: recipe});
-        }
-    })
-});
-
-app.post("/recipes/:id/comments", function(req, res) {
-    //lookup reipe using id
-    Recipe.findById(req.params.id, function(err, recipe) {
-        if (err) {
-            console.log(err);
-            res.redirect("/recipes");
-        } else {
-            Comment.create(req.body.comment, function(err, comment) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    recipe.comments.push(comment);
-                    recipe.save();
-                    res.redirect("/recipes/"+recipe._id);
-                }
-            });
-        }
-    });
-    //create new comment
-    //connect new comment to recipe
-    //redirect to recipe show page
-});
-
-
-
-/***********************************************************
- AUTHENTICATION ROUTES
- **********************************************************/
-//show register form
-app.get("/register",function(req, res){
-    res.render("register");
-});
-
-//sign up
-app.post("/register",function(req, res){
-    var newUser= new User({username:req.body.username});
-    User.register(newUser,req.body.password,function(err,user){
-        if(err){
-            console.log(err);
-            return res.render("register")
-        }
-        passport.authenticate("local")(req, res,function(){
-            res.redirect("/recipes");
-        });
-    });
-});
-
- //show login form
- app.get("/login",function(req,res){
-     res.render("login");
- });
-
- //login
- app.post("/login",passport.authenticate("local",
- {
-    successRedirect:"/recipes",
-    failureRedirect:"/login"
- }) ,function(req,res){
-
-});
-
-//logout
-app.get("/logout",function(req,res){
-    req.logout();
-    res.redirect("/recipes");
-});
-
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
 
 
 /***********************************************************
