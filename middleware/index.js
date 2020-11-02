961bd46c728b7ca13397b78a2c6030e8e35c9cb4
urvashi02sinha <65072659+urvashi02sinha@users.noreply.github.com>
2020-11-02 21:11:49 +0530

    Adding flash
    1).install and  configure connect flash.
    2).Added bootstrap alerts to header.

--- middleware/index.js
+++ middleware/index.js
@@ -49,6 +49,7 @@ middlewareObj.checkRecipeOwnerShip =function(req, res, next){
             if(req.isAuthenticated()){
                 return next();
             }
+            req.flash("error","Please Login First!");
             res.redirect("/login");
     }
     
--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.post("/register",function(req, res){
 
  //show login form
  router.get("/login",function(req,res){
-     res.render("login");
+     res.render("login",{message:req.flash("error")});
  });
 
  //login
