a8e24165236a9dad6361b0ac93039ed5e2e02355
urvashi02sinha <65072659+urvashi02sinha@users.noreply.github.com>
2020-11-03 15:18:28 +0530

    flash messages when error occur
    flashes messages when error occur or successfully registered

--- routes/index.js
+++ routes/index.js
@@ -30,10 +30,12 @@ router.post("/register",function(req, res){
     var newUser= new User({username:req.body.username});
     User.register(newUser,req.body.password,function(err,user){
         if(err){
-            console.log(err);
+            
+            req.flash("error",err.message);
             return res.render("register")
         }
         passport.authenticate("local")(req, res,function(){
+            req.flash("success", "Welcome to AppEtizer " + user.username);
             res.redirect("/recipes");
         });
     });
@@ -62,14 +64,6 @@ router.get("/logout",function(req,res){
 
 
 
-/*********************************************************
-MIDDLEWARE
-**********************************************************/
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+
 
 module.exports = router;
